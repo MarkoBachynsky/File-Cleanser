e6255b9592cbab036c93dfc9d5175ae36bb13c44
Marko <MarkoBachynsky1997@gmail.com>
2018-09-03 23:31:24 -0500

    Add project files.

---
+++ SubDelete/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace SubDelete
+{
+    partial class GUIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUIForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(124, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.cbxDesign_DrawItem);
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectedIndexChangedConfirmed);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 3;
+            this.button1.TabStop = false;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.ForeColor = System.Drawing.Color.Black;
+            this.progressBar1.Location = new System.Drawing.Point(0, 201);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(256, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(132, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "Search Subfolders";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(196, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 23);
+            this.button3.TabIndex = 1;
+            this.button3.TabStop = false;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(114, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(129, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Files Deleted: 0";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(189, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 23);
+            this.button4.TabIndex = 11;
+            this.button4.TabStop = false;
+            this.button4.Text = "Remove";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // GUIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 224);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GUIForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Marko\'s File Cleanser";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.exitProgram);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ SubDelete/Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SubDelete
+{
+    public partial class GUIForm : Form
+    {
+        private Form prompt;
+        private TextBox textBox;
+        private Button add;
+        private int deleted, totalToBeDeleted;
+        public string lastIndex = "";
+
+        public GUIForm()
+        {
+            InitializeComponent();
+            checkAndLoadBinaries();
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+                richTextBox1.Text = DeSerializeObject<Item>(comboBox1.SelectedItem.ToString() + ".bin").Value;
+            }
+            try { textBox1.Text = DeSerializeObject<string>("path.bin");
+            } catch (Exception e) { }
+        }
+
+        private void checkAndLoadBinaries()
+        {
+            // Create Or Find Binary Directory To Make Working Directory
+            if (!Directory.Exists(Path.GetDirectoryName(Application.ExecutablePath) + @"\bin"))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(Application.ExecutablePath) + @"\bin");
+            }
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Application.ExecutablePath) + @"\bin");
+            lastIndex = comboBox1.Text;
+            comboBox1.Items.Clear();
+            // Find all Binaries And Load Them Into Combobox
+            String[] allfiles = Directory.GetFiles(Path.GetDirectoryName(Application.ExecutablePath) + @"\bin", "*bin", SearchOption.AllDirectories);
+            if (allfiles.Length > 0)
+            {
+                button4.Enabled = true;
+                foreach (string path in allfiles)
+                { 
+                    string pathHelper = path.Substring(path.LastIndexOf(@"\") + 1);
+                    string fileName = pathHelper.Substring(0, pathHelper.LastIndexOf("."));
+                    if(!fileName.Equals("path"))
+                    comboBox1.Items.Add( fileName );
+                }
+                comboBox1.SelectedIndex = comboBox1.Items.IndexOf(lastIndex);
+                richTextBox1.Enabled = true;
+            } else
+            {
+                button4.Enabled = false;
+                richTextBox1.Enabled = false;
+            }
+        }
+
+        public void recursoDeleteOoo(string Path, string[] formats)
+        {
+            String[] allfiles;
+            if(checkBox1.CheckState == CheckState.Checked) allfiles = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories);
+            else allfiles = Directory.GetFiles(Path, "*.*", SearchOption.TopDirectoryOnly);
+            int True = 0;
+            totalToBeDeleted = 1;
+            deleted = 0;
+            foreach (string filePath in allfiles)
+            {
+                True = 0;
+                foreach (string format in formats)
+                {
+                    if (filePath.ToLower().EndsWith(format))
+                    {
+                        True = 1;
+                        break;
+                    }
+                }
+                if (File.Exists(filePath) && True == 0)
+                {
+                    totalToBeDeleted++;
+                }
+            }
+
+            foreach (string filePath in allfiles)
+            {
+                True = 0;
+                foreach (string format in formats)
+                {
+                    if (filePath.ToLower().EndsWith(format))
+                    {
+                        True = 1;
+                        break;
+                    }
+                }
+                if (File.Exists(filePath) && True == 0)
+                {
+                    File.Delete(filePath);
+                    deleted++;
+                    label1.Text = "Files Deleted: " + deleted;
+                    progressBar1.Value = (deleted * 100) / totalToBeDeleted;
+                }
+            }
+            progressBar1.Visible = false;
+        }
+
+        private void deleteEmptyDirectory(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                deleteEmptyDirectory(directory);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveBinary();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        public static void SerializeObject<T>(T serializableObject, string fileName)
+        {
+            if (serializableObject == null) { return; }
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+            using (MemoryStream stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, serializableObject);
+                stream.Position = 0;
+                xmlDocument.Load(stream);
+                xmlDocument.Save(fileName);
+                stream.Close();
+            }
+        }
+
+        public static T DeSerializeObject<T>(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) { return default(T); }
+            T objectOut = default(T);
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(fileName);
+            string xmlString = xmlDocument.OuterXml;
+            using (StringReader read = new StringReader(xmlString))
+            {
+                Type outType = typeof(T);
+                XmlSerializer serializer = new XmlSerializer(outType);
+                using (XmlReader reader = new XmlTextReader(read))
+                {
+                    objectOut = (T)serializer.Deserialize(reader);
+                    reader.Close();
+                }
+
+                read.Close();
+            }
+            return objectOut;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin"))
+            {
+                File.Delete(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin");
+            }
+            // Save Item
+            SerializeObject(new Item(comboBox1.Text, richTextBox1.Text), comboBox1.Text + ".bin");
+            string dir = textBox1.Text;
+            progressBar1.Visible = true;
+            this.Enabled = false;
+            recursoDeleteOoo(dir, richTextBox1.Text.Split(new[] { "\n" }, StringSplitOptions.None));
+            deleteEmptyDirectory(dir);
+            this.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            prompt = new Form();
+            prompt.Parent = this.Parent;
+            prompt.StartPosition = FormStartPosition.CenterParent;
+            prompt.MaximizeBox = false;
+            prompt.MinimizeBox = false;
+            prompt.ShowIcon = false;
+            prompt.AutoSize = false;
+            prompt.Width = 190;
+            prompt.Height = 125;
+            prompt.FormBorderStyle = FormBorderStyle.FixedSingle;
+            textBox = new TextBox() { Left = 10, Top = 35, Width = 150 };
+            textBox.TabIndex = 0;
+            textBox.Cursor = Cursors.Default;
+            textBox.Multiline = false;
+            textBox.AutoSize = false;
+            textBox.Cursor = Cursors.IBeam;
+            Label label = new Label() { Text = "Enter extension group name:", Left = 10, Top = 10, Width = 150 };
+            Button cancel = new Button() { Text = "Cancel", Left = 110, Top = 60 };
+            add = new Button() { Text = "Add", Left = 10, Top = 60 };
+            prompt.AcceptButton = add;
+            add.TabIndex = 1;
+            cancel.TabIndex = 2;
+            cancel.Click += new EventHandler(cancel_Click);
+            add.Click += new EventHandler(add_Click);
+            add.Enabled = false;
+            textBox.TextChanged += new System.EventHandler(add_TextChanged);
+            cancel.AutoSize = true;
+            cancel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            cancel.AutoEllipsis = false;
+            add.AutoSize = true;
+            add.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            add.AutoEllipsis = false;
+            prompt.Controls.Add(label);
+            prompt.Controls.Add(cancel);
+            prompt.Controls.Add(add);
+            prompt.Controls.Add(textBox);
+            prompt.ShowDialog();
+
+        }
+
+        private void saveBinary()
+        {
+            if (!lastIndex.Equals(""))
+            {
+                if (File.Exists(Directory.GetCurrentDirectory() + @"\" + lastIndex + ".bin"))
+                {
+                    File.Delete(Directory.GetCurrentDirectory() + @"\" + lastIndex + ".bin");
+                }
+                // Save Item
+                SerializeObject(new Item(lastIndex, richTextBox1.Text), lastIndex + ".bin");
+                richTextBox1.Text = "";
+
+            }
+            lastIndex = comboBox1.Text;
+            checkAndLoadBinaries();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            // Add New Item
+            saveBinary();
+            SerializeObject(new Item(textBox.Text, ""), textBox.Text + ".bin");
+            comboBox1.Items.Add(textBox.Text);
+            comboBox1.SelectedIndex = comboBox1.Items.IndexOf(textBox.Text);
+            lastIndex = comboBox1.Text;
+            richTextBox1.Text = "";
+            richTextBox1.Enabled = true;
+            button4.Enabled = true;
+            prompt.Close();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            prompt.Close();
+        }
+
+        private void comboBox1_SelectedIndexChangedConfirmed(object sender, EventArgs e)
+        {
+            saveBinary();
+            if (File.Exists(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin"))
+            {
+                // Load new Item
+                richTextBox1.Text = DeSerializeObject<Item>(comboBox1.Text + ".bin").Value;
+            }
+
+        }
+
+        private void add_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox.Text.Equals("")      ||
+                textBox.Text.Length >= 248   ||
+                textBox.Text.EndsWith(" ")   ||
+                textBox.Text.EndsWith(".")   ||
+                textBox.Text.Contains("<")   ||
+                textBox.Text.Contains(">")   ||
+                textBox.Text.Contains(":")   ||
+                textBox.Text.Contains("\"")  ||
+                textBox.Text.Contains(@"/")  ||
+                textBox.Text.Contains(@"\")  ||
+                textBox.Text.Contains("|")   ||
+                textBox.Text.Contains("?")   ||
+                textBox.Text.Contains("*")  ||
+                textBox.Text.Equals("path")) add.Enabled = false;
+            else add.Enabled = true;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SerializeObject<String>(textBox1.Text, "path.bin");
+            try
+            {
+                label1.Text = "Files Deleted: " + "0";
+            }
+            catch (ArgumentException er) { }
+
+        }
+
+        // Allow Combo Box to center aligned
+        private void cbxDesign_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            // By using Sender, one method could handle multiple ComboBoxes
+            ComboBox cbx = sender as ComboBox;
+            if (cbx != null)
+            {
+                // Always draw the background
+                e.DrawBackground();
+
+                // Drawing one of the items?
+                if (e.Index >= 0)
+                {
+                    // Set the string alignment.  Choices are Center, Near and Far
+                    StringFormat sf = new StringFormat();
+                    sf.LineAlignment = StringAlignment.Center;
+                    sf.Alignment = StringAlignment.Center;
+
+                    // Set the Brush to ComboBox ForeColor to maintain any ComboBox color settings
+                    // Assumes Brush is solid
+                    Brush brush = new SolidBrush(cbx.ForeColor);
+
+                    // If drawing highlighted selection, change brush
+                    if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                        brush = SystemBrushes.HighlightText;
+
+                    // Draw the string
+                    e.Graphics.DrawString(cbx.Items[e.Index].ToString(), cbx.Font, brush, e.Bounds, sf);
+                }
+            }
+        }
+
+        private void exitProgram(object sender, FormClosedEventArgs e)
+        {
+            if (comboBox1.Items.Count <= 0) return;
+            if (File.Exists(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin"))
+            {
+                File.Delete(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin");
+            }
+            // Save Item
+            SerializeObject(new Item(comboBox1.Text, richTextBox1.Text), comboBox1.Text + ".bin");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int currentIndex = comboBox1.SelectedIndex;
+            if (File.Exists(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin"))
+            {
+                File.Delete(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin");
+            }
+            comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+            if (comboBox1.Items.Count <= 0)
+            {
+                button4.Enabled = false;
+                richTextBox1.Enabled = false;
+                return;
+            }
+            comboBox1.SelectedIndex = (currentIndex - 1);
+            if (comboBox1.SelectedIndex < 0) comboBox1.SelectedIndex = 0;
+            richTextBox1.Text = "";
+            if (File.Exists(Directory.GetCurrentDirectory() + @"\" + comboBox1.Text + ".bin"))
+            {
+                // Load new Item
+                richTextBox1.Text = DeSerializeObject<Item>(comboBox1.Text + ".bin").Value;
+            }
+
+        }
+
+    }
+
+    public class Item
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public Item()
+        {
+            Key = "";
+            Value = "";
+        }
+
+        public Item(string n, string f)
+        {
+            Key = n;
+            Value = f;
+        }
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
+
